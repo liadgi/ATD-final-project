a1e34be077973fb07c9822a6ff769cf5401726af
Idan Mosseri <mosseridan@gmail.com>
2017-08-15 13:27:58 +0300

    top recipes

--- angular-src/src/app/components/top-recipes/top-recipes.component.ts
+++ angular-src/src/app/components/top-recipes/top-recipes.component.ts
@@ -11,7 +11,7 @@ import {Http, Response} from '@angular/http';
 })
 
 
-export class TopRecipesComponent implements OnInit, OnDestroy {
+export class TopRecipesComponent implements OnInit {
   messages = [];
   connection;
   message;
@@ -20,89 +20,12 @@ export class TopRecipesComponent implements OnInit, OnDestroy {
   constructor(private postsService: PostsService, private socketioService: SocketioService) { }
 
   ngOnInit() {
-
-    // this.connection = this.socketioService.getMessages('top-posts').subscribe((post) => {
-    //   console.log('1!!!!got: ',post);
-    // });
-
-    // this.connection = this.socketioService.getMessages('message').subscribe((message) => {
-    //   console.log('2!!!!got: '+message);
-    //   this.messages.push(message);
-    // });
-    
-    // this.connection = this.socketioService.getMessages('post').subscribe(message => {
-    //   console.log("POSTPOST!!!");
-    // });
-    // this.connection = this.socketioService.getMessages('profile').subscribe(message => {
-    //   console.log("POROFILEPROFILE!!!");
-    // });
-
-
-    // //check if the user is logged in
-    // if (this.auth.loggedIn()) {
-    //   //for most Liked Recipes
-    //   let send = {user_id: this.auth.getLogoedInUser()["_id"]};
-    //   console.log("send", send);
-    //   this.http
-    //   //post the form data to the url defined above and map the response. Then subscribe //to initiate the post. if you don't subscribe, angular wont post.
-    //     .post('http://127.0.0.1:3001/users/LikedPost/', send).map((res: Response) => res.json()).subscribe(
-    //     //map the success function and alert the response
-    //     (success) => {
-    //       console.log(success.msg);
-    //       this.mostLikedRecipes = success.msg;
-
-    //     },
-    //     (error) => alert(error));
-
-    //   // for most Like Users
-    //   let send2 = {user_id: this.auth.getLogoedInUser()["_id"]};
-    //   console.log("send", send2);
-    //   this.http
-    //   //post the form data to the url defined above and map the response. Then subscribe //to initiate the post. if you don't subscribe, angular wont post.
-    //     .post('http://127.0.0.1:3001/users/LikedUsers', send2).map((res: Response) => res.json()).subscribe(
-    //     //map the success function and alert the response
-    //     (success) => {
-    //       console.log("hello", success.msg, typeof success.msg);
-    //       this.mostLikeUsers = success.msg;
-
-    //     },
-    //     (error) => alert(error));
-
-    // }
-
-    // this.connection = this.socketioService.getMessages('post').subscribe(message => {
-    //     location.reload();
-    //     //console.log("get emit from the server");
-    //     let send = {post_id:this.thisPost["_id"]};
-    //     this.http.post('http://127.0.0.1:3001/posts/getSinglePost', send).map((res: Response) => res.json()).subscribe(
-    //       //map the success function and alert the response
-    //       (success) => {
-    //         console.log(success.msg);
-    //         this.thisPost=success.msg;
-    //         location.reload();
-    //       },
-    //       (error) => alert(error));
-
-
-    //   });
-
-
-
     this.postsService.loadPosts('top');
   }
 
-  ngOnDestroy() {
-    this.connection.unsubscribe();
-  }
 
   pageChanged(page) {
     this.postsService.loadPosts('top', page);
   }
-
-  sendMessage(){
-    this.socketioService.sendMessage('add-message',this.message);
-    this.message = '';
-  }
-
 
 }
